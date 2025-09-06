f056f2c9aec1417f1d1efaaf2ee719dd22718d5b
Slava <sv060522@yandex.ru>
2025-09-07 02:20:45 +0300

    ДЗ 12 (файл matAB.txt изменён так как примерах в задаче вначале вводится n и потом подряд 2 одинаковые матрицы, а в файле matAB.txt перед второй матрицей тоже был указан размер, из-за чего результат с тем же кодом программы уже не получится получить. Поэтому я удалил число 10 между 2 матрицами в файле.)

---
+++ Task12.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int A[10][10], B[10][10], C[10][10] = {0};
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
